dd10f5e346cc694b65048dee63995518b0dfa9d1
Hoang Do <hoangdo.dh94@gmail.com>
2015-10-13 00:53:11 +0700

    update

--- js/controllers.js
+++ js/controllers.js
@@ -1,42 +1,42 @@
 var controllersModule = angular.module('dkmhApp.Controllers', []);
 
-controllersModule.controller('DKMHAppController',
-    ['$scope', '$rootScope', 'AuthService',
-    function($scope, $rootScope, AuthService){
-        $scope.doSignOut = function(){
+controllersModule.controller('DKMHAppController', ['$scope', '$rootScope', 'AuthService',
+    function($scope, $rootScope, AuthService) {
+        $scope.doSignOut = function() {
             AuthService.signOut();
         }
-    }]);
+    }
+]);
 
-controllersModule.controller('RegisterController', ['$scope', 'AuthService', function($scope, AuthService){
-    $scope.doRegister = function(){
+controllersModule.controller('RegisterController', ['$scope', 'AuthService', function($scope, AuthService) {
+    $scope.doRegister = function() {
         AuthService.register($scope.registerEmail, $scope.registerPassword);
     }
 }]);
 
-controllersModule.controller('SignInController', ['$scope', 'AuthService', function($scope, AuthService){
-    $scope.doSignIn = function(){
+controllersModule.controller('SignInController', ['$scope', 'AuthService', function($scope, AuthService) {
+    $scope.doSignIn = function() {
         AuthService.signIn($scope.signInEmail, $scope.signInPassword, false);
     }
 }]);
 
-controllersModule.controller('MyItemsController', ['$scope', '$rootScope', '$firebaseArray', function($scope, $rootScope, $firebaseArray){
+controllersModule.controller('MyItemsController', ['$scope', '$rootScope', '$firebaseArray', function($scope, $rootScope, $firebaseArray) {
     var firebaseRef = new Firebase('https://dkmh-online-auction.firebaseio.com');
 
     $scope.myItems = $firebaseArray(firebaseRef.child('items').orderByChild("ownerId").equalTo($rootScope.currentUser.uid));
     console.log('$scope.myItems');
     console.log($scope.myItems);
 
-    angular.element('#item-image').change(function(e){
+    angular.element('#item-image').change(function(e) {
         var imageFile = this.files[0];
         var fileReader = new FileReader();
         fileReader.readAsDataURL(imageFile);
-        fileReader.onloadend = function () {
+        fileReader.onloadend = function() {
             $scope.itemImage = fileReader.result;
         }
     });
 
-    $scope.addNewItem = function(){
+    $scope.addNewItem = function() {
         var expiredDate = new Date($scope.itemExpiredDate).getTime();
         var item = {
             name: $scope.itemName,
@@ -57,13 +57,13 @@ controllersModule.controller('MyItemsController', ['$scope', '$rootScope', '$fir
     };
 }]);
 
-controllersModule.controller('MainController', ['$scope', '$rootScope', '$firebaseArray', '$location', function($scope, $rootScope, $firebaseArray, $location){
+controllersModule.controller('MainController', ['$scope', '$rootScope', '$firebaseArray', '$location', function($scope, $rootScope, $firebaseArray, $location) {
     var firebaseRef = new Firebase('https://dkmh-online-auction.firebaseio.com');
 
     $scope.allItems = $firebaseArray(firebaseRef.child('items').orderByChild("expiredDate").limitToLast(4));
 
-    $scope.setCurrentItem = function(itemId, itemName, itemNewPrice, itemCurrentPrice){
-        if (!$rootScope.currentUser){
+    $scope.setCurrentItem = function(itemId, itemName, itemNewPrice, itemCurrentPrice) {
+        if (!$rootScope.currentUser) {
             swal({
                 title: 'Not signed-in',
                 text: 'Please sign in before bidding for items !',
@@ -72,31 +72,40 @@ controllersModule.controller('MainController', ['$scope', '$rootScope', '$fireba
                 console.log('after error');
                 $location.path('/sign-in');
             });
-        } else if (itemNewPrice > $rootScope.currentUser.balance){
+        } else if (itemNewPrice > $rootScope.currentUser.balance) {
             swal({
                 title: 'Not enough budget',
                 text: 'Please deposit before continuing.',
                 type: 'error'
             });
-        } else if (itemNewPrice <= itemCurrentPrice){
+        } else if (itemNewPrice <= itemCurrentPrice) {
             swal({
                 title: 'Invalid value',
                 text: 'Please bid a value greater than current item price !',
                 type: 'error'
             });
-        }
-        else {
+        } else {
             $scope.currentItem = {
                 id: itemId,
                 name: itemName,
                 value: itemNewPrice
             };
-            // $('#submit-bid-modal').modal('show');
+            swal({
+                title: "Are you sure?",
+                text: 'Submit bid for <b>' +$scope.currentItem.itemName+ '</b> for <b>$' +$scope.currentItem.value+ '</b>?',
+                type: 'info',
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, Place my Bid!",
+                closeOnConfirm: true
+            }, function() {
+                $scope.submitBid();
+            });
         }
 
     };
 
-    $scope.submitBid = function(){
+    $scope.submitBid = function() {
         firebaseRef.child('items').child($scope.currentItem.id).update({
             currentPrice: $scope.currentItem.value,
             currentBuyerId: $rootScope.currentUser.uid,
@@ -106,15 +115,15 @@ controllersModule.controller('MainController', ['$scope', '$rootScope', '$fireba
     };
 }]);
 
-controllersModule.controller('DepositController', ['$scope', '$rootScope', function($scope, $rootScope){
+controllersModule.controller('DepositController', ['$scope', '$rootScope', function($scope, $rootScope) {
     var firebaseRef = new Firebase('https://dkmh-online-auction.firebaseio.com');
 
-    $scope.updateDeposit = function(depositAmount){
+    $scope.updateDeposit = function(depositAmount) {
         var newBalance = $rootScope.currentUser.balance + depositAmount;
         firebaseRef.child('users').child($rootScope.currentUser.uid).update({
             balance: newBalance
-        }, function(err){
-            if (err){
+        }, function(err) {
+            if (err) {
                 $rootScope.errorMsg = err;
             } else {
                 $rootScope.successMsg = 'Deposit successfully !';
